d6eecc8c7a80364da82b21b072c9d979b2471424
Administrator <https://github.com/1075440322/test.git>
2019-07-04 16:26:54 +0800

    test

---
+++ Test.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Administrator on 2019/7/4 0004.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.print("11111111111111111111");
+    }
+}
